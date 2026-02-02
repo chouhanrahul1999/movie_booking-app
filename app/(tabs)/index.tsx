6d92147456636db01f12c329b8aaaa751a412ea6
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-02 23:15:23 +0530

    fixed the css isue on homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -31,51 +31,45 @@ const Index = () => {
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full h-full z-0" />
 
-      <ScrollView
-        className="flex-1 px-5"
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ minHeight: "100%", padding: 10 }}
-      >
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-
-        {moviesLoading ? (
-          <ActivityIndicator
-            size="large"
-            color="#0000ff"
-            className="mt-10 self-center"
-          />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
-        ) : (
-          <View className="flex-1 mt-5">
-            <SearchBar
-              onPress={() => router.push("/search")}
-              placeholder="Search for movies"
-            />
-
-            <>
+      {moviesLoading ? (
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          className="mt-10 self-center"
+        />
+      ) : moviesError ? (
+        <Text className="text-white text-center mt-10">
+          Error: {moviesError?.message}
+        </Text>
+      ) : (
+        <FlatList
+          data={movies}
+          ListHeaderComponent={() => (
+            <View className="pt-10">
+              <Image
+                source={icons.logo}
+                className="w-12 h-10 mt-20 mb-6 mx-auto"
+              />
+              <SearchBar
+                onPress={() => router.push("/search")}
+                placeholder="Search for movies"
+              />
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movies
               </Text>
-
-              <FlatList
-                data={movies}
-                renderItem={({ item }) => (<MovieCard {...item} />)}
-                keyExtractor={(item) => item.id.toString()}
-                numColumns={3}
-                columnWrapperStyle={{
-                  justifyContent: "flex-start",
-                  gap: 20,
-                  paddingRight: 5,
-                  marginBottom: 10,
-                }}
-                className="mt-2 pb-32"
-                scrollEnabled={false}
-              />
-            </>
-          </View>
-        )}
-      </ScrollView>
+            </View>
+          )}
+          renderItem={({ item }) => <MovieCard {...item} />}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={3}
+          columnWrapperStyle={{
+            justifyContent: "space-between",
+            marginBottom: 10,
+          }}
+          className="flex-1 px-5"
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      )}
     </View>
   );
 };
