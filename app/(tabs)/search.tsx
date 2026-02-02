d01caba907b3995cb2987c63c29de574d20667d7
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-02 23:43:44 +0530

    created search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,68 @@
+import { images } from "@/constants/images";
+import { fetchPopularMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+import MovieCard from "../components/MovieCard";
+import { icons } from "@/constants/icons";
+import SearchBar from "../components/SearchBar";
 
 const Search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchPopularMovies({
+      query: "",
+    }),
+  );
+
   return (
-    <View>
-      <Text> textInComponent </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className="px-5 pt-10"
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          gap: 16,
+          marginVertical: 8,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar placeholder="Search movies..." onPress={() => {}} />
+            </View>
+
+            {moviesLoading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {moviesError && (
+              <Text className="text-red-500 text-center my-3">
+                Error: {moviesError.message}
+              </Text>
+            )}
+          </>
+        }
+      />
     </View>
   );
 };
